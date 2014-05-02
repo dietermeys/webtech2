f5fef50da72a672a974f364e34e876f91595df0d
Dieter Meys <dietermeys@gmail.com>
2014-05-02 10:00:10 +0200

    pagina's toegevoegd

--- Project_IMDWall/Project_webtech/app.js
+++ Project_IMDWall/Project_webtech/app.js
@@ -1,59 +1,38 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var port = 2014;
+var app = require('express')();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+//var mysql = require('mysql');
+console.log('Listening on port ' + port);
+server.listen(port);
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-/// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/ask.html');
+});
+app.get('/ask.html', function (req, res) {
+  res.sendfile(__dirname + '/ask.html');
+});
+app.get('/wall.html', function (req, res) {
+  res.sendfile(__dirname + '/wall.html');
+});
+app.get('/moderate.html', function (req, res) {
+  res.sendfile(__dirname + '/moderate.html');
+});
+/*
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : ''
 });
 
-/// error handlers
+connection.connect();
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  console.log('The solution is: ', rows[0].solution);
 });
 
-
+connection.end();
+*/
 module.exports = app;
